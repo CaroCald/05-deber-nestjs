f40ef983ca2a9d41cd431aed65098572070cd466
CaroCald <carolina.2594@hotmail.com>
2018-05-13 14:01:39 -0500

    archivo revisado

--- project/src/inicio.controller.ts
+++ project/src/inicio.controller.ts
@@ -3,12 +3,12 @@ const fs = require('fs');
 @Controller()
 export class InicioController{
     @Get('Home')
-    mostrar(@Req()request){
+    mostrar(@Res()response){
         let htmlcontenido=fs.readFileSync(__dirname+'/html/contenido.html', 'utf8');
         let htmlheader= fs.readFileSync(__dirname+'/html/header.html', 'utf8');
         let htmlfooter= fs.readFileSync(__dirname+'/html/footer.html', 'utf8');
-        let variable= htmlcontenido+htmlheader+htmlfooter;
-        return request.send(variable);
+        let variable= htmlheader+htmlcontenido+htmlfooter;
+        return response.status(200).send(variable);
     }
 
 }
