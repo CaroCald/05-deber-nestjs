122108eb3da5f9b41072157d51c1e96b25e17e76
CaroCald <carolina.2594@hotmail.com>
2018-05-14 20:57:44 -0500

    revisado

--- project/src/preguntasFrecuentes.controller.ts
+++ project/src/preguntasFrecuentes.controller.ts
@@ -3,15 +3,15 @@ const fs = require('fs');
 let htmlpreguntas= fs.readFileSync(__dirname+'/html/preguntasFrecuentes.html', 'utf8');
 @Controller()
 export class PreguntasFrecuentesController{
-    preguntasF: preguntasFrecuentes[]=[];
+    preguntas: preguntasFrecuentes[]=[];
 
     @Post('anadirPreguntas')
     anadirPreguntas(@Req() req, @Res() res){
         const parametrosQuery=req.query;
-        this.preguntasF.push(new preguntasFrecuentes(parametrosQuery.pregunta, parametrosQuery.respuesta));
+        this.preguntas.push(new preguntasFrecuentes(parametrosQuery.pregunta, parametrosQuery.respuesta));
             htmlpreguntas=htmlpreguntas.concat('<h1> Pregunta </h1> ',parametrosQuery.pregunta);
             htmlpreguntas=htmlpreguntas.concat('<p> Respuesta</p>', parametrosQuery.respuesta);
-        return res.send(this.preguntasF);
+        return res.send(this.preguntas);
     }
 
     @Get('preguntas')
